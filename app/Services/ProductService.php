2c08aad42ca856473ff59c02c89b941a2c1f7a11
offgridengineering <sid@off-grid-engineering.com>
2018-03-04 09:16:57 +1000

    Refactor DB calls

--- app/Services/ProductService.php
+++ app/Services/ProductService.php
@@ -19,8 +19,46 @@ use App\Models\Product;
 class ProductService
 {
 
+	public static function insertArray( $request)
+	{
+		$rv = 0;
+		$o = new Product;
+		$o->prod_sku = $request['prod_sku'];
+		$o->prod_title = $request['prod_title'];
+		$o->prod_short_desc = $request['prod_short_desc'];
+		$o->prod_long_desc = $request['prod_long_desc'];
+		$o->prod_visible = $request['prod_visible'];
+		$o->prod_weight = $request['prod_weight'];
+		$o->prod_qty = $request['prod_qty'];
+		$o->prod_reorder_qty = $request['prod_reorder_qty'];
+		$o->prod_base_cost = $request['prod_base_cost'];
+		$o->prod_retail_cost = $request['prod_retail_cost'];
+		$o->prod_combine_code = $request['prod_combine_code'];
+		$o->prod_type = $request['prod_type'];
+		$o->prod_has_free_shipping = $request['prod_has_free_shipping'];
+		
+		$o->prod_date_created = date("Y-m-d");
+		$o->prod_time_created = date("H:i:s");
+		$o->prod_date_valid_from = date("Y-m-d");
+		$o->prod_date_valid_to = date("Y-m-d");
+		if(($o->save()) >0)
+		{
+			\Session::flash('flash_message','Product Saved!');
+			return $o->id;
+		}
+		else
+		{
+			\Session::flash('flash_error','Product failed to Save!');
+			return 0;
+		}
+	}
+
+
+
+
+
 	/**
-	 * Insert a new row and return the row id or error.
+	 * Insert a new row and return the row id or 0 on error.
 	 *
 	 * @param	ProductRequest	Request object
 	 * @return	integer
@@ -47,15 +85,16 @@ class ProductService
 		$o->prod_time_created = date("H:i:s");
 		$o->prod_date_valid_from = date("Y-m-d");
 		$o->prod_date_valid_to = date("Y-m-d");
-		if(($rv=$o->save()) >0)
+		if($o->save() > 0)
 		{
 			\Session::flash('flash_message','Product Saved!');
+			return $o->id;
 		}
 		else
 		{
 			\Session::flash('flash_error','Product failed to Save!');
 		}
-		return $rv;
+		return 0;
 	}
 
 
--- app/Services/StoreService.php
+++ app/Services/StoreService.php
@@ -1,6 +1,10 @@
 <?php
 /**
- * \class StoreService
+ * \class	StoreService
+ * @author	Sid Young	<sid@off-grid-engineering.com>
+ * @date	2017-06-30
+ *
+ *
  */
 namespace App\Services;
 
@@ -15,54 +19,69 @@ use App\Models\Store;
  */
 class StoreService
 {
-
-	public static function getData(StoreRequest $d)
-	{
-		$r = array(
-			'store_env_code'=>$d['store_env_code'],
-			'store_name'=>$d['store_name'],
-			'store_url'=>$d['store_url'],
-			'store_currency'=>$d['store_currency'],
-			'store_status'=>$d['store_status'],
-			'store_parent_id'=>$d['store_parent_id'],
-			'store_logo_filename'=>$d['store_logo_filename'],
-			'store_logo_alt_text'=>$d['store_logo_alt_text'],
-			'store_logo_thumb'=>$d['store_logo_thumb'],
-			'store_logo_invoice'=>$d['store_logo_invoice'],
-			'store_logo_email'=>$d['store_logo_email'],
-			'store_hours'=>$d['store_hours'],
-			'store_sales_email'=>$d['store_sales_email'],
-			'store_address'=>$d['store_address'],
-			'store_address2'=>$d['store_address2'],
-			'store_contact'=>$d['store_contact'],
-			'store_bg_image'=>$d['store_bg_image']
-			);
-	}
-
-
 	public static function insert(StoreRequest $request)
 	{
-		$Store = new Store;
-		$d = array($request->except('_token') );
-		$rv = $Store->InsertStore( $d[0] );
-		if($rv > 0)
+		$o = new Store;
+		$o->store_env_code = $request['store_env_code'];
+		$o->store_name = $request['store_name'];
+		$o->store_url = $request['store_url'];
+		$o->store_currency = $request['store_currency'];
+		$o->store_status = $request['store_status'];
+		$o->store_parent_id = $request['store_parent_id'];
+		$o->store_logo_filename = $request['store_logo_filename'];
+		$o->store_logo_alt_text = $request['store_logo_alt_text'];
+		$o->store_logo_thumb = $request['store_logo_thumb'];
+		$o->store_logo_invoice = $request['store_logo_invoice'];
+		$o->store_logo_email = $request['store_logo_email'];
+		$o->store_hours = $request['store_hours'];
+		$o->store_sales_email = $request['store_sales_email'];
+		$o->store_address = $request['store_address'];
+		$o->store_address2 = $request['store_address2'];
+		$o->store_contact = $request['store_contact'];
+		$o->store_bg_image = $request['store_bg_image'];
+		if($o->save() >0)
 		{
 			\Session::flash('flash_message','Store Saved!');
+			return $o->id;
 		}
-		return $rv;
+		else
+		{
+			\Session::flash('flash_error','ERROR - Store insert failed!');
+			return 0;
+		}
 	}
 
 
 
 	public static function update(StoreRequest $request)
 	{
-		$Store = new Store;
-		$d = array($request->except('_token') );
-		$rv = $Store->UpdateStore( $d[0] );
-		if($rv > 0)
+		$o = Store::find($request['id']);
+		$o->store_env_code = $request['store_env_code'];
+		$o->store_name = $request['store_name'];
+		$o->store_url = $request['store_url'];
+		$o->store_currency = $request['store_currency'];
+		$o->store_status = $request['store_status'];
+		$o->store_parent_id = $request['store_parent_id'];
+		$o->store_logo_filename = $request['store_logo_filename'];
+		$o->store_logo_alt_text = $request['store_logo_alt_text'];
+		$o->store_logo_thumb = $request['store_logo_thumb'];
+		$o->store_logo_invoice = $request['store_logo_invoice'];
+		$o->store_logo_email = $request['store_logo_email'];
+		$o->store_hours = $request['store_hours'];
+		$o->store_sales_email = $request['store_sales_email'];
+		$o->store_address = $request['store_address'];
+		$o->store_address2 = $request['store_address2'];
+		$o->store_contact = $request['store_contact'];
+		$o->store_bg_image = $request['store_bg_image'];
+
+		if($o->save() > 0)
 		{
 			\Session::flash('flash_message','Store updated successfully!');
 		}
-		return $rv;
+		else
+		{
+			\Session::flash('flash_error','ERROR - Store update failed!');
+			return 0;
+		}
 	}
 }
