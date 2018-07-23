8c47ffef594cef01f8b314751e15f26701948800
root <root@vps1.p-and-f-holdings.com>
2018-07-23 16:54:46 +1000

    Cleanup code, removed email sending and refactored to be a placeholder for business logic

--- app/Jobs/OrderDispatched.php
+++ app/Jobs/OrderDispatched.php
@@ -51,35 +51,12 @@ protected $store;
 
 
 /**
- * The email address to send the Subscription Confirmation to.
- * @var string $to
+ * The email address 
+ * @var string $email
  */
-protected $to;
+protected $email;
 
 
-protected $from;
-
-
-/**
- * The subject line for this email
- * @var string $subject
- */
-protected $subject;
-
-
-/**
- * The filename and path for email template to use.
- * Note: Template may also be made of SEOHelper calls.
- * @var string template_file_name
- */
-protected $template_file_name;
-
-
-/**
- * Tempalte to use
- * @var string $ACTION
- */
-private $ACTION="ORDER_DISPATCHED";
 
 
 
@@ -87,31 +64,16 @@ private $ACTION="ORDER_DISPATCHED";
      * Create a new job instance initialize mail transport and save store and email details away.
      * Also fetch relevant template using ORDER_DISPATCHED action 
 	 *
-     * @param	mixed	$store	store data collection
+     * @param	mixed	$store	The Store object
      * @param	string	$email	email address of customer
-     * @param	mixed	$order
+     * @param	mixed	$order	The Order object
      * @return	void
      */
     public function __construct($store, $email, $order)
     {
 		$this->store = $store;
 		$this->order = $order;
-
-		$this->to    = $email;
-		$this->from  = $store->store_sales_email;
-
-		#
-		# Add order number to the SUbject and details from the order in the handle() method
-		#
-		$number = sprintf("%08d",$order->id);
-		$this->subject = "Order #".$number." Dispatched";
-
-		$this->template_file_name = $this->getTemplate($this->ACTION);
-		echo "Template: ". $this->template_file_name. "</br>";
-		if(strlen($this->template_file_name)==0)
-		{
-			$this->template_file_name = strtolower($this->ACTION).".email";
-		}
+		$this->email = $email;
     }
 
 
@@ -124,40 +86,12 @@ private $ACTION="ORDER_DISPATCHED";
      */
     public function handle()
     {
-		$Customer = new Customer;
-		$Order = new Order;
-		$Store = new Store;
-		$customer = Customer::find($this->order->order_cid);
-
-		$path = base_path();
-		$file = $path."/templates/".$this->store->store_env_code."/".$this->template_file_name;
-
-		$text = "<h1>WARNING</h1><p>Order Dispatched email NOT sent to: ".$this->to."</p><br/><br/>Check Template file! - <b>".$file."</b>";
-		if(file_exists($file))
-		{
-			$body = file_get_contents($file);
-	
-			$store_name_lower = strtoupper($this->store->store_name);
-			$store_name = str_replace(" ","_", $store_name_lower);
-			$header_tag = $store_name."_EMAIL_HEADER";
-			$footer_tag = $store_name."_EMAIL_FOOTER";
-
-			$header = SEOHelper::getText($header_tag);
-			$footer = SEOHelper::getText($footer_tag);
-
-			$t1 = $header.$body.$footer;
-			$t2 = $Customer->translate($t1, $customer);
-			$t3 = $Order->translate($t2, $this->order);
-			$text = $Store->translate($t3, $this->store);
-
-			$cmd = new EmailUserJob($this->to, $this->from, $this->subject, $text);
-			dispatch($cmd);
-		}
+		$text = "Notice: Order ".$this->order->id." Dispatched to: ".$this->email;
+		$subject = "[LARVELA] Order Dispatched email sent to [".$this->email."]";
+		$from = $this->store->store_sales_email;
 
 		$admin_user = Customer::find(1);
-		$admin_email = $admin_user->customer_email;
-		$cmd = new EmailUserJob($admin_email, $this->from, "[LARVELA] Order Dispatched email sent to [".$this->to."]", $text);
-		dispatch($cmd);
+		dispatch( new EmailUserJob($admin_user->customer_email, $from, $subject, $text));
 
     }
 }
