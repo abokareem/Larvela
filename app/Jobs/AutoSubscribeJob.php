12c4e9860f9fce15e41d6d986e93a331ab35ba27
root <root@vps1.p-and-f-holdings.com>
2018-07-23 21:08:57 +1000

    Clean code and document. Added return code for insert

--- app/Jobs/AutoSubscribeJob.php
+++ app/Jobs/AutoSubscribeJob.php
@@ -2,31 +2,38 @@
 /**
  * \class	AutoSubscribeJob
  * \author	Sid Young <sid@off-grid-engineering.com>
- * \date	2017-07-27
+ * \date	2018-07-23
  *
  *
- *
- * [CC]
+ * \addtogroup Subscription
+ * Subscribe the new Customer to the Subscriptions table and
+ * provide a place where additional business logic can be called.
  */
 namespace App\Jobs;
 
-
-use Hash;
 use App\Jobs\Job;
 
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Contracts\Events\Dispatcher as DispatcherContract;
 
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+
+
 use App\Models\SubscriptionRequest;
 
 
-
 /**
- * \brief When a customer is added, automatically add them to the subscribers table
+ * \brief Subscribe the new Customer to the Subscriptions table and
+ * provide a place where additional business logic can be called.
  */
-class AutoSubscribeJob extends Job
+class AutoSubscribeJob implements ShouldQueue
 {
-private $ACTION="AUTO_SUBSCRIBE_JOB";
+use InteractsWithQueue, Queueable, SerializesModels;
+
 
 
 
@@ -34,10 +41,9 @@ private $ACTION="AUTO_SUBSCRIBE_JOB";
      * Create a new job instance initialize mail transport and save store and email details away.
      * Also fetch relevant template using CONFIRM_SUBSCRIPTION action 
 	 *
-     * @param  $store     mixed - store data collection
-     * @param  $email     string - email address of customer
-     * @param  $hash_url  string - hashed URL string to substitue in.
-     * @return void
+     * @param 	mixed	$store	- The Store object.
+     * @param	string	$email	- The customer email address.
+     * @return	void
      */
     public function __construct($store, $email)
     {
@@ -47,7 +53,7 @@ private $ACTION="AUTO_SUBSCRIBE_JOB";
 
 
     /**
-	 * Nothing to do here.
+	 * Place Holder for additional Business Logic
 	 *
      * @return void
      */
@@ -68,11 +74,13 @@ private $ACTION="AUTO_SUBSCRIBE_JOB";
 	protected function SubscribeEmail( $email )
 	{
 		$today = date("Y-m-d");
-		$SubscriptionRequest = new SubscriptionRequest;
-		$data = array('sr_email'=>$email,'sr_status'=>"C", 'sr_process_value'=>0,
-			'sr_date_created'=>$today,
-			'sr_date_updated'=>$today
-			);
-		return $SubscriptionRequest->InsertData( $data );
+		$o = new SubscriptionRequest;
+		$o->sr_email = $email;
+		$o->sr_status = "C";
+		$o->sr_process_value = 0;
+		$o->sr_date_created = date("Y-m-d");
+		$o->sr_date_updated= date("Y-m-d");
+		$o->save();
+		return $o->id; 
 	}
 }
