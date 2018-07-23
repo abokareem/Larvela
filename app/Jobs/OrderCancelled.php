df4ef12f1be159efe3752f801d5e851be84012a1
root <root@vps1.p-and-f-holdings.com>
2018-07-23 11:25:15 +1000

    Refactored to be queue-able and remove email code. Now a placeholder for additional busienss logic

--- app/Jobs/OrderCancelled.php
+++ app/Jobs/OrderCancelled.php
@@ -7,158 +7,81 @@
  *
  * [CC]
  * 
- * \addtogroup Transactional
- * OrderCancelled - An order has been cancelled, send an email.
+ * \addtogroup Orders
+ * OrderCancelled - An order has been cancelled so this Job provides a point to do additional work.
  */
 namespace App\Jobs;
 
 
 use App\Jobs\Job;
-use App\Helpers\SEOHelper;
 
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Contracts\Events\Dispatcher as DispatcherContract;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
 
 use App\Models\Store;
 use App\Models\Customer;
 use App\Models\Order;
 
-use App\Jobs\EmailUserJob;
-use App\Traits\TemplateTrait;
-
-
 
 
 /**
- * \brief Send a templated email to customer informing them the their order has been cancelled.
- * {INFO_2017-10-28} Moved template to ./store_env_code/...
+ * \brief Provide a point to do additional work when an  order has been cancelled.
  */
-class OrderCancelled extends Job
+class OrderCancelled implements ShouldQueue
 {
-use TemplateTrait;
-
+use InteractsWithQueue, Queueable, SerializesModels;
 
 
 /**
- * The store object from the database tables stores
+ * The Store object for the currently selected Store.
  * @var mixed $store
  */
 protected $store;
 
 
 /**
- * The order object from the database table "orders"
+ * The email of the customer who the order belongs to.
+ * @var string $email
+ */
+public $email;
+
+
+/**
+ * The Order object from the database table "orders"
  * @var mixed $order
  */
 protected $order;
 
 
-/**
- * The email address to send the Subscription Confirmation to.
- * @var string $to
- */
-protected $to;
-
-protected $from;
-
-
-/**
- * The subject line for this email
- * @var string $subject
- */
-protected $subject;
-
-
-/**
- * The filename and path for email template to use.
- * Note: Template may also be made of SEOHelper calls.
- * @var string template_file_name
- */
-protected $template_file_name;
-
-
-private $ACTION="ORDER_CANCELLED";
-
-
     /**
-     * Create a new job instance initialize mail transport and
-	 * save store and email details away.
-     * Also fetch relevant template using "$ACTION" 
+     * Create a new job instance
+	 * - Save Store, email and Order details.
 	 *
-     * @param  mixed	$store	Store data (object)
-     * @param  string	$email 	email address of customer
+     * @param  mixed	$store	The current Store.
+     * @param  string	$email 	email address of customer.
+     * @param  mixed	$order	The Order object.
      * @return void
      */
     public function __construct($store, $email, $order)
     {
 		$this->store = $store;
+		$this->email = $email;
 		$this->order = $order;
-
-		$this->to = $email;
-		$this->subject = "Order Cancelled";
-		$this->from = $this->store->store_sales_email;
-
-		$this->template_file_name = $this->getTemplate($this->ACTION);
-		if(strlen($this->template_file_name)==0)
-		{
-			$this->template_file_name = strtolower($this->ACTION).".email";
-		}
     }
 
 
 
     /**
-     * Fetch the template, parse with the store helper and
-	 * then send using Swift Mailer. 
-	 *
-	 * {FIX_2017-11-05} - OrderCancelled.php - removed call to StrReplace
+	 * Place holder for additonal business logic
 	 *     
      * @return void
      */
     public function handle()
     {
-		$Customer = new Customer;
-		$Store = new Store;
-		$Order = new Order;
-		$customer = Customer::find($this->order->order_cid);
-
-		$reason = "not specified - contact store";
-		if($this->order->order_status == "C")
-		{
-			$reason = "Manually Cancelled - contact store for details";
-		}
-		if(($this->order->order_status == "W") && ($this->order->order_payment_status="W"))
-		{
-			$reason = "Payment not received in allotted time";
-		}
-
-		$path = base_path();
-		$file = $path."/templates/".$this->store->store_env_code."/".$this->template_file_name;
-		$text = "<h1>WARNING</h1><p>Order Cancelled email NOT sent to: ".$this->to."</p><br/><br/>Check Template file! - <b>".$file."</b>";
-		if(file_exists($file))
-		{
-			$body = file_get_contents($file);
-
-			$store_name_lower = strtoupper($this->store->store_name);
-			$store_name = str_replace(" ","_", $store_name_lower);
-			$header_tag = $store_name."_EMAIL_HEADER";
-			$footer_tag = $store_name."_EMAIL_FOOTER";
-
-			$header = SEOHelper::getText($header_tag);
-			$footer = SEOHelper::getText($footer_tag);
-		
-			$t1 = $header.$body.$footer;
-			$t2 = $Customer->translate($t1, $customer);
-			$t3 = $Order->translate($t1, $this->order);
-			$text = $Store->translate($t2, $this->store);
-		
-			$cmd = new EmailUserJob($this->to, $this->from, $this->subject, $text);
-			dispatch($cmd);
-		}
-
-		$admin_user = Customer::find(1);
-		$admin_email = $admin_user->customer_email;
-		$cmd = new EmailUserJob($admin_email, $this->from, "[LARVELA] Order Cancelled message sent to [".$this->to."]", $text);
-		dispatch($cmd);
     }
 }
