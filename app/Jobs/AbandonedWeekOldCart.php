97300ac7e8b01cce0f62868f60e50988dbf988e3
root <root@vps1.p-and-f-holdings.com>
2018-07-23 11:10:57 +1000

    Refactored email code out and left empty for additional Business Logic

--- app/Jobs/AbandonedWeekOldCart.php
+++ app/Jobs/AbandonedWeekOldCart.php
@@ -6,8 +6,9 @@
  *
  *
  *
- * \addtogroup	CartAbandonment
- * AbandonedWeekOldCart - The cart has been abandond for a week, so email user to entice them back to the store.
+ * \addtogroup	CRON
+ * AbandonedWeekOldCart - Place holder for additonal business Logic.
+ * - Cart has been abandoned for 1 week now.
  */
 namespace App\Jobs;
 
@@ -23,65 +24,36 @@ use App\Jobs\EmailUserJob;
 
 
 use App\Models\Customer;
-use App\Models\Users;
-use App\Models\Store;
-use App\Models\Cart;
-use App\Models\CartItem;
-use App\Models\Product;
 
 
-use App\Traits\TemplateTrait;
-use App\Traits\Logger;
 
 
 /**
- * \brief Send a templated email after the cart has been abandoned for a week
- * {INFO_2017-10-28} Moved template to ./store_env_code/...
- * {INFO_2017-11-05} Added $from class var
+ * \brief Cart has been abandoned for 1 week, execute any additional business logic here.
  */
 class AbandonedWeekOldCart extends Job 
 {
-use TemplateTrait;
-use Logger;
-
 
 /**
- * The store object from the database tables stores, has all the details about the seelcted store.
- * @var object $store
+ * The Store object 
+ * @var mixed $store
  */
 protected $store;
 
 
 /**
- * The email address to send the Subscription Confirmation to.
- * @var string $to
+ * The email address of the customer
+ * @var string $email
  */
-protected $to;
+protected $email;
 
 
 /**
- * From address for this email
- * @var string $from
+ * The Cart object
+ * @var mixed $cart
  */
-protected $from;
+protected $cart;
 
-
-/**
- * The subject line for this email
- * @var string $subject
- */
-protected $subject;
-
-
-/**
- * The filename and path for email template to use.
- * Note: Template may also be made of SEOHelper calls.
- * @var string template_file_name
- */
-protected $template_file_name;
-
-
-private $ACTION="CART_7DAY_ABANDONED";
 
 
 
@@ -94,18 +66,11 @@ private $ACTION="CART_7DAY_ABANDONED";
      * @param  $hash_url  string - hashed URL string to substitue in.
      * @return void
      */
-    public function __construct($email, $cart, $store)
+    public function __construct($store, $email, $cart)
     {
-		$this->to = $email;
-		$this->cart = $cart;
 		$this->store = $store;
-		$this->from = $store->store_sales_email;
-		$this->subject = "Cart Abandoned!";
-		$this->template_file_name = $this->getTemplate($this->ACTION);
-		if(strlen($this->template_file_name)==0)
-		{
-			$this->template_file_name = strtolower($this->ACTION).".email";
-		}
+		$this->email = $email;
+		$this->cart  = $cart;
     }
 
 
@@ -117,34 +82,11 @@ private $ACTION="CART_7DAY_ABANDONED";
      */
     public function handle()
     {
-		$Customer = new Customer;
-		$Store = new Store;
-		
-		$path = base_path();
-		$file = $path."/templates/".$this->store->store_env_code."/".$this->template_file_name;
-		$text = "<h1>WARNING</h1><p>Abandond Cart email NOT sent to: ".$this->to."</p><br/><br/>Check Template file! - <b>".$file."</b>";
-		if(file_exists($file))
-		{
-			$body = file_get_contents($file);
-	
-			$store_name_lower = strtoupper($this->store->store_name);
-			$store_name = str_replace(" ","_", $store_name_lower);
-			$header_tag = $store_name."_EMAIL_HEADER";
-			$footer_tag = $store_name."_EMAIL_FOOTER";
-	
-			$header = SEOHelper::getText($header_tag);
-			$footer = SEOHelper::getText($footer_tag);
-	
-			$t1 = $header.$body.$footer;
-			$text = $Store->translate($t1, $this->store);
-	
-			$cmd = new EmailUserJob($this->to, $this->from, $this->subject, $text);
-   			dispatch($cmd);
-		}
+		$from = $this->store->store_sales_email;	
+		$subject = "[LARVELA] Week Old Abandoned cart message sent to [".$this->email."]";
+		$text = "Notice: Cart ".$this->cart->id." abandoned 1 week ago by customer ".$this->email;
 
 		$admin_user = Customer::find(1);
-		$admin_email = $admin_user->customer_email;
-		$cmd = new EmailUserJob($admin_email, $this->from, "[LARVELA] Week Old Abandoned cart message sent to [".$this->to."]", $text);
-		dispatch($cmd);
+		dispatch(new EmailUserJob($admin_user->customer_email, $from, $subject, $text));
     }
 }
