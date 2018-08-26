222bb9ec06e317086ac4f8ddd96c5cb96f0a5988
root <root@vps1.p-and-f-holdings.com>
2018-08-26 14:23:09 +1000

    Updated Routes

--- routes/install-routes.php
+++ routes/install-routes.php
@@ -7,5 +7,7 @@
 Route::post('/install/save/1','Installer@SaveAdminDetails');
 Route::post('/install/save/2','Installer@SaveStoreBasic');
 Route::post('/install/save/3','Installer@SaveStoreDetails');
+Route::post('/install/prev/1','Installer@ShowAdminPage');
+Route::post('/install/prev/2','Installer@ShowStorePage');
 
 
