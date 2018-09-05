59216c74163342ba4e76316c96bfeef2f70c8826
root <root@vps1.p-and-f-holdings.com>
2018-09-05 10:34:01 +1000

    Added method overridewith store variable passthrough

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -36,4 +36,11 @@ class LoginController extends Controller
     {
         $this->middleware('guest', ['except' => 'logout']);
     }
+
+
+	public function showLoginForm()
+	{
+	    $store = app('store');
+		return view('auth.login',['store'=>$store]);
+	}
 }
