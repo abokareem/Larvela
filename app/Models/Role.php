8edeb1bfb6153940c82f194d98984084e3b6bd3b
root <root@vps1.p-and-f-holdings.com>
2018-09-10 23:11:26 +1000

    Updated license and version

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -1,11 +1,31 @@
 <?php
 /**
  * \class	Role
- * @author	Sid Young <sid@off-grid-engineering.com>
- * @date	2016-07-29
+ * \author	Sid Young <sid@off-grid-engineering.com>
+ * \date	2016-07-29
+ * \version	1.0.1
  *
  *
- * [CC]
+ * Copyright 2018 Sid Young, Present & Future Holdings Pty Ltd
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the 
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
  */
 namespace App\Models;
 
@@ -25,7 +45,7 @@ class Role extends Model
  * name of table
  * @var string $table
  */
-protected $table = 'role';
+protected $table = 'roles';
 
 
 	/**
@@ -38,17 +58,5 @@ protected $table = 'role';
     {
         return $this->hasMany('App\User', 'role_id', 'id');
     }
-
-
-	/**
-	 * Given a string role name, return all rows that match
-	 *
-	 * @param	string	$r	the role name
-	 * @return	mixed
-	 */
-	public function getByRole($r)
-	{
-		return \DB::table('role')->where(['name'=>$r])->get();
-	}
 
 }
