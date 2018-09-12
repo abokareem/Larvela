20bab0e64673e61af0722af2dfa03a8a8995cf3f
root <root@vps1.p-and-f-holdings.com>
2018-09-12 23:00:14 +1000

    Added attribues() to model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,9 +3,29 @@
  * \class	Product
  * \author	Sid Young <sid@off-grid-engineering.com>
  * \date	2016-07-29
+ * \version	1.0.0
  *
  *
- * [CC]
+ * Copyright 2018 Sid Young, Present & Future Holdings Pty Ltd
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the 
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
  */
 namespace App\Models;
 
@@ -61,6 +81,11 @@ protected $fillable = ['prod_sku','prod_title','prod_short_desc','prod_long_desc
 		return $this->belongsToMany('App\Models\Category');
 	}
 
+
+	public function attributes()
+	{
+		return $this->belongsToMany('App\Models\Attribute');
+	}
 
 	
 	/**
