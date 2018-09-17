8153cf36ab4638712eac2cf70c75e3a91145d23c
root <root@vps1.p-and-f-holdings.com>
2018-09-18 09:44:09 +1000

    Added extra column called token

--- database/seeds/ProductTypeSeeder.php
+++ database/seeds/ProductTypeSeeder.php
@@ -13,9 +13,10 @@ class ProductTypeSeeder extends Seeder
     public function run()
     {
 		DB::table('product_types')->truncate();
-		ProductType::create(['id'=>1,'product_type'=>'Basic Product']);
-		ProductType::create(['id'=>2,'product_type'=>'Parent Product']);
-		ProductType::create(['id'=>3,'product_type'=>'Virtual Product (Limited)']);
-		ProductType::create(['id'=>4,'product_type'=>'Virtual Product (Unlimited)']);
+		ProductType::create(['id'=>1,'product_type'=>'Basic Product','product_type_token'=>'BASIC']);
+		ProductType::create(['id'=>2,'product_type'=>'Parent Product','product_type_token'=>'PARENT']);
+		ProductType::create(['id'=>3,'product_type'=>'Virtual Product (Limited)','product_type_token'=>'VLIMITED']);
+		ProductType::create(['id'=>4,'product_type'=>'Virtual Product (Unlimited)','product_type_token'=>'VUNLIMITED']);
+		ProductType::create(['id'=>5,'product_type'=>'Pack Product','product_type_token'=>'PACK']);
     }
 }
