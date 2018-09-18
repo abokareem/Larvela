bfb37a69acda347f092a0d5b56f55ce7bd0f0a94
root <root@vps1.p-and-f-holdings.com>
2018-09-18 12:04:40 +1000

    Changed getImages() to just return the parent images

--- app/Services/ImageService.php
+++ app/Services/ImageService.php
@@ -1,15 +1,40 @@
 <?php
+/**
+ * \class	ImageService
+ * \author	Sid Young <sid@off-grid-engineering.com>
+ * \date	2018-09-17
+ *
+ *
+ * Copyright 2018 Sid Young, Present & Future Holdings Pty Ltd
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the 
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
 namespace App\Services;
 
 use App\Models\Image;
 
-
+/**
+ * \brief This service rovides a range of commonly used image retrieval methods.
+ */
 class ImageService
 {
-	function __construct()
-	{
-
-	}
 
 
 	/**
@@ -40,7 +65,7 @@ class ImageService
 
 	/**
 	 * Given the product, get all images.
-	 * - parent images and thumbnails.
+	 * - parent images only!
 	 *
 	 * @param	App\Models\Product	$product
 	 * @return	array
@@ -54,11 +79,11 @@ class ImageService
 			foreach($mapping as $m)
 			{
 				array_push($images,$m);
-				$pp_images = Image::where('image_parent_id',$m->id)->get();
-				foreach($pp_images as $pi)
-				{
-					array_push($images,$pi);
-				}
+				#$pp_images = Image::where('image_parent_id',$m->id)->get();
+				#foreach($pp_images as $pi)
+				#{
+				#	array_push($images,$pi);
+				#}
 			}
 		}
 		return $images;
@@ -66,6 +91,13 @@ class ImageService
 
 
 
+	/**
+	 * Return an array of thumbnails of all the main product images.
+	 * - Thumbnails are usually in 3 sizes and have a parent ID that is not zero.
+	 * - Parent images are defined in pivot table "image_product"
+	 *
+	 * @return	array
+	 */
 	public static function getThumbnails($product)
 	{
 		$thumbnails = array();
