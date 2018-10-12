15b0145da59730f4bb663ef00e837a8323758f84
root <root@vps1.p-and-f-holdings.com>
2018-10-12 23:05:33 +1000

    Corrected incorrect column name

--- database/seeds/AttributeValuesTableSeeder.php
+++ database/seeds/AttributeValuesTableSeeder.php
@@ -13,9 +13,9 @@ class AttributeValuesTableSeeder extends Seeder
     public function run()
     {
 		DB::table('attribute_values')->truncate();
-		AttributeValue::create([ 'id'=>1, 'attr_id'=>1,'attrib_value'=>'BLACK','attr_sort_index'=>1]);
-		AttributeValue::create([ 'id'=>2, 'attr_id'=>1,'attrib_value'=>'WHITE','attr_sort_index'=>2]);
-		AttributeValue::create([ 'id'=>2, 'attr_id'=>1,'attrib_value'=>'RED','attr_sort_index'=>3]);
-		AttributeValue::create([ 'id'=>2, 'attr_id'=>1,'attrib_value'=>'BLUE','attr_sort_index'=>4]);
+		AttributeValue::create([ 'id'=>1, 'attr_id'=>1,'attr_value'=>'BLACK','attr_sort_index'=>1]);
+		AttributeValue::create([ 'id'=>2, 'attr_id'=>1,'attr_value'=>'WHITE','attr_sort_index'=>2]);
+		AttributeValue::create([ 'id'=>2, 'attr_id'=>1,'attr_value'=>'RED','attr_sort_index'=>3]);
+		AttributeValue::create([ 'id'=>2, 'attr_id'=>1,'attr_value'=>'BLUE','attr_sort_index'=>4]);
     }
 }
