00ae949e696ae7318b97caa8a98582bcbfef793a
root <root@vps1.p-and-f-holdings.com>
2018-10-12 23:01:40 +1000

    Corrected references to Store

--- database/seeds/AttributeValuesTableSeeder.php
+++ database/seeds/AttributeValuesTableSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use App\Models\Store;
+use App\Models\AttributeValue;
 
 class AttributeValuesTableSeeder extends Seeder
 {
@@ -13,9 +13,9 @@ class AttributeValuesTableSeeder extends Seeder
     public function run()
     {
 		DB::table('attribute_values')->truncate();
-		Store::create([ 'id'=>1, 'attr_id'=>1,'attrib_value'=>'BLACK','attr_sort_index'=>1]);
-		Store::create([ 'id'=>2, 'attr_id'=>1,'attrib_value'=>'WHITE','attr_sort_index'=>2]);
-		Store::create([ 'id'=>2, 'attr_id'=>1,'attrib_value'=>'RED','attr_sort_index'=>3]);
-		Store::create([ 'id'=>2, 'attr_id'=>1,'attrib_value'=>'BLUE','attr_sort_index'=>4]);
+		AttributeValue::create([ 'id'=>1, 'attr_id'=>1,'attrib_value'=>'BLACK','attr_sort_index'=>1]);
+		AttributeValue::create([ 'id'=>2, 'attr_id'=>1,'attrib_value'=>'WHITE','attr_sort_index'=>2]);
+		AttributeValue::create([ 'id'=>2, 'attr_id'=>1,'attrib_value'=>'RED','attr_sort_index'=>3]);
+		AttributeValue::create([ 'id'=>2, 'attr_id'=>1,'attrib_value'=>'BLUE','attr_sort_index'=>4]);
     }
 }
