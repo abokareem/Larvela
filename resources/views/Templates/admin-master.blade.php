942b3cc497678dd2cffa20f4db58aa3c0f9afcbb
root <root@vps1.p-and-f-holdings.com>
2018-10-13 16:07:41 +1000

    Updated master administration template and removed css link from oage

--- resources/views/Templates/admin-master.blade.php
+++ resources/views/Templates/admin-master.blade.php
@@ -8,7 +8,7 @@
 
 <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.css">
 <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
-<link href="/css/store.css" rel="stylesheet">
+<!-- link href="/css/{{$THEME_NAME}}/larvela.css" rel="stylesheet" -->
 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 <script src="//npmcdn.com/isotope-layout@3.0/dist/isotope.pkgd.min.js"></script>
@@ -16,11 +16,11 @@
 </head>
 <body>
 <!-- Inject Navigation Content -->
-@if( view()->exists('nav-admin') )
-    @include('nav-admin')
+@if( view()->exists('Templates.nav-admin') )
+    @include('Templates.nav-admin')
 @endif
 <div class="container-fluid">
-	<h1>Administration Console</h1>
+	@yield('header')
 	@yield('content')
 </div>
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
