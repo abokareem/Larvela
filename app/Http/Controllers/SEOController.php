6a46eee73b22f016f45456c03d73a7b827eb4820
root <root@vps1.p-and-f-holdings.com>
2018-10-13 22:35:55 +1000

    Fixed edit flag on Save()

--- app/Http/Controllers/SEOController.php
+++ app/Http/Controllers/SEOController.php
@@ -1,10 +1,30 @@
 <?php
 /**
  * \class	SEOController
- * @author	Sid Young <sid@off-grif-engineering.com>
- * @date	2016-08-23
+ * \author	Sid Young <sid@off-grif-engineering.com>
+ * \date	2016-08-23
  *
- * [CC]
+ * 
+ * Copyright 2018 Sid Young, Present & Future Holdings Pty Ltd
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the 
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
  *
  * \addtogroup SEO
  * SEOController - The SEO Controller is responsible for managing SEO content.
@@ -86,7 +106,7 @@ class SEOController extends Controller
 			$o->seo_token = $request['seo_token'];
 			$o->seo_html_data = $request['seo_html_data'];
 			$o->seo_status = $request['seo_status'];
-			$o->seo_edit = $request['seo_edit'];
+			$o->seo_edit = "Y";
 			$o->seo_store_id = $request['seo_store_id'];
 			if($o->save()>0)
 			{
