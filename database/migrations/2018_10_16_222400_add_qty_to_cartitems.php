4bf59eaa1865a4d08fe9f3fae80a3ffece54c64b
root <root@vps1.p-and-f-holdings.com>
2018-10-16 22:25:52 +1000

    Added qty column

---
+++ database/migrations/2018_10_16_222400_add_qty_to_cartitems.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddQtyToCartitems extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+		Schema::table('cart_items', function($table)
+		{
+			$table->integer('customer_address')->unsigned();
+		});
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+		Schema::table('cart_items', function($table)
+		{
+			$table->dropColumn('qty');
+		});
+    }
+}
