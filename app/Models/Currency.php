ef3bd8b080a4f395493044b8fea5656e1ca16cbf
root <root@vps1.p-and-f-holdings.com>
2018-11-03 13:03:09 +1000

    Added new table for currencies

---
+++ app/Models/Currency.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * \class	Currency
+ * \date	2018-11-03
+ * \author	Sid Young <sid@off-grid-engineering.com>
+ * \version	1.0.0
+ *
+ *
+ * Copyright 2018 Sid Young, Present & Future Holdings Pty Ltd
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the 
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+
+
+/**
+ * \brief MVC Model for Country table
+ */
+class Currency extends Model
+{
+
+
+/**
+ * The table this model uses
+ * @var string $table
+ */
+protected $table = "currencies";
+
+
+/**
+ * No timestamps used in DB table
+ * @var	boolean	$timestamps
+ */
+public $timestamps = false;
+
+
+/**
+ * columns that are mass-assignable
+  * @var string $fillable
+   */
+   protected $fillable = ['id','currency_code','currency_name','currency_numeric'];
+
+}
