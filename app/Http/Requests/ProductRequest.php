d530efddb34bfed56137306d436bff23cddd7653
root <root@vps1.p-and-f-holdings.com>
2018-11-13 23:01:25 +1000

    Added support methods inside Request object

--- app/Http/Requests/ProductRequest.php
+++ app/Http/Requests/ProductRequest.php
@@ -1,19 +1,46 @@
-<?php namespace App\Http\Requests;
+<?php
+/**
+ * \class	ProductRequest
+ * \version	1.0.4
+ *
+ *
+ * Copyright 2018 Sid Young, Present & Future Holdings Pty Ltd
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the 
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ *
+ */
+namespace App\Http\Requests;
 
 use App\Http\Requests\Request;
 use App\Services\ProductService;
+use App\Services\CategoryService;
+use App\Services\AttributeService;
 use Illuminate\Foundation\Http\FormRequest;
 
 
-
+/**
+ * \brief Custom request class to process form data and
+ * provide common methods for processing Product data.
+ */
 class ProductRequest extends FormRequest
 {
-public function test() { dd($this);}
-
-
-
-
-
 	/**
 	 * Save the newly created product and return its ID
 	 *
@@ -21,7 +48,29 @@ public function test() { dd($this);}
 	 */
 	public function SaveProduct()
 	{
-		return ProductService::insert($this);
+		$product_id = ProductService::insert($this);
+		if($product_id > 0)
+		{
+			CategoryService::AssignCategories($this, $product_id);
+		}
+		return $product_id;
+	}
+
+
+	/**
+	 * Save the product attributes for the given product ID
+	 *
+	 * @param	integer	$product_id
+	 * @return	void
+	 */
+	public function SaveProductAttributes($product_id)
+	{
+		$empty = array();
+		$attributes = $this->input('attributes', $empty );
+		if(sizeof($attributes)>0)
+		{
+			AttributeService::AssignAttributes($attributes,$product_id);
+		}
 	}
 
 
@@ -34,7 +83,9 @@ public function test() { dd($this);}
 	public function UpdateProduct($product_id)
 	{
 		$this->request->add(['id', $product_id]);
-		return ProductService::update($this);
+		$count = ProductService::update($this);
+		CategoryService::AssignCategories($this, $product_id);
+		return $count;
 	}
 
 
