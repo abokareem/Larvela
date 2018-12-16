d7afa7ad1c55accf03c8fe6bca1e7ddd86e62914
root <root@vps1.p-and-f-holdings.com>
2018-12-16 12:17:03 +1000

    reformatted footer for general release

--- resources/views/Mail/DEMO/footer.blade.php
+++ resources/views/Mail/DEMO/footer.blade.php
@@ -1,44 +1,15 @@
 <table align="center" border="0" cellpadding="0" cellspacing="0" style="background-color:transparent; border-color:transparent" width="100%">
 	<tbody>
 	<tr>
-		<td align="center" bgcolor="#ff5e6c" colspan="5" style="background-color:rgb(255,94,108)">
-				<a href="https://" target="_blank" title="contact-us">
-					<img alt="CONTACT" border="0" height="27" src="https://rockabillydames.com/rockabillydames-mobile.jpg" width="133" style="background:transparent;">
-				</a>
-				<a href="https://www.facebook.com/Rockabilly-Dames-170810546337919/" target="_blank" title="facebook">
-					<img alt="FACEBOOK" border="0" height="113" src="http://cdn-au.mailsnd.com/49074/_lf2OxWkTumKQpCY1e9Ua30rxMShCoqgNeCoN2Qdt0U/2462713.jpg" width="54">
-				</a>
-				<a href="https://"  target="_blank" title="twitter">
-					<img alt="TWITTER" border="0" height="113" src="http://cdn-au.mailsnd.com/49074/85QStc6lmEtdlwNFkqhXhTB5Ep77am8vUPQREpqnoHQ/2462715.jpg" width="51">
-				</a>
-				<a href=""  target="_blank" title="instagram">
-					<img alt="2462712.jpg" border="0" height="113" src="http://cdn-au.mailsnd.com/49074/pu55NlL2jFOqQ0UecSA4L1z7mD-JYIuJS5rP6sjacHY/2462712.jpg" width="48">
-				</a>
-				<a href="" target="_blank" title="pinterest">
-					<img alt="PINTEREST" border="0" height="113" src="http://cdn-au.mailsnd.com/49074/-71M0VhpCJjexnqesTkNSpPV4Po-mePeTgGjsyIxULI/2462716.jpg" width="49">
-				</a>
-				<a href="" target="_blank" title="youtube">
-					<img alt="YOUTUBE" border="0" height="113" src="http://cdn-au.mailsnd.com/49074/T2TYfaX96IVEKivSN_2iyBhjhutvpMrRHtGLJdeNiPk/2462710.jpg" width="99">
-				</a>
-		</td>
-	</tr>
-	<tr>
-		<td align="center" colspan="5" style="background-color:transparent">
-			<a href="https://rockabillydames.com/"  target="_blank" title="rockabillydames">
-				<img alt="Rockabilly Dames" border="0" height="76" src="https://rockabillydames.com/rockabillydameslogo.jpg" width="470">
-			</a>
-		</td>
-	</tr>
-	<tr>
 		<td align="center" colspan="5">
 			<font style="font-size:11px">
 				<font face="Helvetica, Arial, sans-serif">Don't miss our emails! Add&nbsp;</font>
 			</font>
 			<font>
 				<font face="Trebuchet MS, Helvetica, sans-serif">
-					<a href="mailto:sales@rockabillydames.com" title="sales@rockabillydames.com">
+					<a href="mailto:{{$store->store_sales_email}}" title="{{$store->store_sales_email}}">
 						<font style="font-size:11px">
-							<font face="Helvetica, Arial, sans-serif">sales@rockabillydames.com</font>
+							<font face="Helvetica, Arial, sans-serif">{{$store->store_sales_email}}</font>
 						</font>
 					</a>
 				</font>
@@ -58,7 +29,7 @@
 			<tr>
 				<td align="center">
 					<font color="#000000" face="Verdana, Arial, Helvetica, sans-serif" style="font-size:10px">
-						<a href="https://rockabillydames.com/subscription/manage"  target="_blank">
+						<a href="{{ $store->store_url }}/subscription/manage"  target="_blank">
 							<font color="#000000" face="Arial, Helvetica, sans-serif" style="font-size:10px">Manage your Subscriptions</font>
 						</a>
 					</font> | <a href="{{ $store->store_url }}/unsubscribe?{{ $hash }}"  target="_blank">
@@ -77,7 +48,7 @@
 	<tr>
 		<td>
 			<p align="center">
-				<font face="Verdana, Arial, Helvetica, sans-serif" color="#555555" size="1">This email was sent by {{$store->store_name }}, PO Box 611, Ashgrove, QLD, 4060, Australia<br>to: {{ $email }}
+				<font face="Verdana, Arial, Helvetica, sans-serif" color="#555555" size="1">This email was sent by {{$store->store_name }}, {{ $store->store_address }}, {{$store->store_address2}}, {{$store->store_country}}<br>to: {{ $email }}
 				</font>
 			</p>
 			<p align="center">
