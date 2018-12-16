bd778bfa83dbfd9479e6070befd0eefeccdda012
root <root@vps1.p-and-f-holdings.com>
2018-12-16 12:18:50 +1000

    reformatted header for general release

--- resources/views/Mail/DEMO/header.blade.php
+++ resources/views/Mail/DEMO/header.blade.php
@@ -4,20 +4,8 @@
 			<tr>
 				<td align="center" colspan="5" style="background-color:transparent">
 					<a href="{{ $store->store_url }}"  target="_blank" title="Larvela Demo Store.">
-						<img alt="Larvela Demo Site" border="0" height="76" src="https://localhost.com/logo.jpg" width="470">
+						<img alt="Larvela Demo Site" border="0" height="76" src="{{ $store->store_url}}/logo.jpg" width="470">
 					</a>
-				</td>
-			</tr>
-			<tr>
-				<td align="center" colspan="5" style="background-color:transparent">
-					<div align="right"><br>
-						<a href="{{ $store->store_url }}/returns"  target="_blank" title="return-guarantee">
-							<img alt="365 Day Returns" border="0" height="27" src="{{ $store->store_url }}/mobile.jpg" width="133">
-						</a><br>
-						<a href="{{ $store->store_url }}/returns"  target="_blank" title="delivery-guarantee">
-							<img alt="free delivery on orders over $100" border="0" height="31" src="{{ $store->store_url }}/free-delivery.jpg" width="295">
-						</a>
-					</div>
 				</td>
 			</tr>
 		</tbody>
