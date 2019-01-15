addc95ec3a9d9ba625ea3675be922eaa285f71d3
root <root@vps1.p-and-f-holdings.com>
2019-01-15 16:31:40 +1000

    Cleaned up Insert and Update routines to use traits

--- app/Http/Controllers/Admin/AdminStoreController.php
+++ app/Http/Controllers/Admin/AdminStoreController.php
@@ -3,7 +3,7 @@
  * \class	AdminStoreController
  * \author	Sid Young <sid@off-grid-engineering.com>
  * \date	2018-08-22
- * \version	1.0.4
+ * \version	1.0.5
  *
  *
  * Copyright 2018 Sid Young, Present & Future Holdings Pty Ltd
@@ -175,35 +175,7 @@ use StoreInsertTrait;
 	public function SaveNewStore(StoreRequest $request)
 	{
 		$this->LogFunction("SaveNewStore()");
-		
-		$o = new Store;
-		$o->store_env_code = $request['store_env_code'];
-		$o->store_name = $request['store_name'];
-		$o->store_url = $request['store_url'];
-		$o->store_currency = $request['store_currency'];
-		$o->store_status = $request['store_status'];
-		$o->store_parent_id = $request['store_parent_id'];
-		$o->store_logo_filename = $request['store_logo_filename'];
-		$o->store_logo_alt_text = $request['store_logo_alt_text'];
-		$o->store_logo_thumb = $request['store_logo_thumb'];
-		$o->store_logo_invoice = $request['store_logo_invoice'];
-		$o->store_logo_email = $request['store_logo_email'];
-		$o->store_hours = $request['store_hours'];
-		$o->store_sales_email = $request['store_sales_email'];
-		$o->store_address = $request['store_address'];
-		$o->store_address2 = $request['store_address2'];
-		$o->store_contact = $request['store_contact'];
-		$o->store_bg_image = $request['store_bg_image'];
-		if($o->save() >0)
-		{
-			\Session::flash('flash_message','Store Saved!');
-			return $o->id;
-		}
-		else
-		{
-			\Session::flash('flash_error','ERROR - Store insert failed!');
-			return 0;
-		}
+		$this->InsertStoreTrait($request);	
 		return $this->ShowStoresPage();
 	}
 }
