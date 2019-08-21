f262f78a85911687550e238656484ebe3af4d94e
root <root@vps1.p-and-f-holdings.com>
2019-08-21 22:20:39 +1000

    Changed parameters

--- config/database.php
+++ config/database.php
@@ -69,8 +69,8 @@ return [
 		'testing'=>[
 			'driver'=>'mysql',
 			'host'=>env('DB_TEST_HOST', 'localhost'),
-			'database'=>env('DB_TEST_DATABASE', 'larvela'),
-			'username'=>env('DB_TEST_USERNAME', 'root'),
+			'database'=>env('DB_TEST_DATABASE', 'testing'),
+			'username'=>env('DB_TEST_USERNAME', 'travis'),
 			'password'=>env('DB_TEST_PASSWORD', ''),
 			'charset'=>'utf8',
 			'collation'=>'utf8_unicode_ci',
